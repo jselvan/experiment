69f3e35e8d60e98ef9ac97384f8cc0aa85d94de4
Janahan Selvanayagam <me@janahan.ca>
2026-01-06 18:28:35 +0000

    implement reward adapter without progress bars

--- experiment/experiments/adapters/RewardAdapter.py
+++ experiment/experiments/adapters/RewardAdapter.py
@@ -3,18 +3,31 @@ from experiment.experiments.adapters.TimeCounter import TimeCounter
 from experiment.experiments.adapters.ProgressBarAdapter import ProgressBarAdapter
 from experiment.renderers.base import Renderer
 from experiment.events import Event
-from typing import Sequence, TYPE_CHECKING
+from typing import Literal, Sequence, TYPE_CHECKING, TypedDict, Optional
 
 if TYPE_CHECKING:
     from experiment.manager import Manager
 
+class ProgressParams(TypedDict):
+    position: tuple[int, int]
+    size: tuple[int, int]
+    colour: tuple[int, int, int]
+    gap: int
+
 class RewardAdapter(BaseAdapter):
+    DEFAULT_PROGRESS_PARAMS: ProgressParams = {
+        'position': (100, 100),
+        'size': (200, 30),
+        'colour': (0, 255, 0),
+        'gap': 10
+    }
     def __init__(self, 
         duration: float,
         n_pulses: int = 1,
         interpulse_interval: float | None = None,
+        children: Sequence[BaseAdapter] = [],
+        progress_params: Optional[ProgressParams] | Literal['none'] = None,
         **kwargs):
-        children = kwargs.pop('children', [])
         super().__init__(children=children)
         self.reward_setup_callback = kwargs.pop('reward_setup_callback', lambda: None)
         self.reward_on_callback = kwargs.pop('reward_on_callback', lambda: None)
@@ -23,7 +36,9 @@ class RewardAdapter(BaseAdapter):
         if interpulse_interval is None:
             interpulse_interval = duration
 
-        progress_params = kwargs.get('progress_params', {})
+        if progress_params is None or progress_params == 'none':
+            progress_params = self.DEFAULT_PROGRESS_PARAMS
+
         progress_center = progress_params.get('position', (100, 100))
         progress_size = progress_params.get('size', (200, 30))
         progress_colour = progress_params.get('colour', (0, 255, 0))
@@ -31,20 +46,26 @@ class RewardAdapter(BaseAdapter):
         total_height = n_pulses * progress_size[1] + (n_pulses - 1) * progress_gap
         progress_topleft = progress_center[0] - progress_size[0] // 2, progress_center[1] - total_height // 2
         offset = progress_size[1] + progress_gap
-
+    
         self.timecounters = []
         for pulse in range(n_pulses):
-            position = (progress_topleft[0], progress_topleft[1] + pulse * offset)
-            self.timecounters.append(
-                TimeCounter(duration, 
-                    on_enter=self.reward_on_callback, 
-                    on_exit=self.reward_off_callback, 
-                    children=[ProgressBarAdapter(
+            if progress_params != 'none':
+                position = (progress_topleft[0], progress_topleft[1] + pulse * offset)
+                children = [
+                    ProgressBarAdapter(
                         position=position,
                         size=progress_size,
                         colour=progress_colour,
                         duration=duration
-                    )]
+                    )
+                ]
+            else:
+                children = []
+            self.timecounters.append(
+                TimeCounter(duration, 
+                    on_enter=self.reward_on_callback, 
+                    on_exit=self.reward_off_callback, 
+                    children=children
                 )
             )
             if pulse < n_pulses - 1:
@@ -82,6 +103,9 @@ class RewardAdapter(BaseAdapter):
         return self.timecounters[self.timecounter_index]
 
     def update(self, tick: float, events: Sequence["Event"]) -> bool:
+        for child in self.children:
+            child.update(tick, events)
+
         if self.current_timecounter is None:
             self.active = False
             return False
@@ -99,7 +123,7 @@ class RewardAdapter(BaseAdapter):
         return False
     
     def render(self, renderer: "Renderer"):
-        for child in self.timecounters:
+        for child in self.timecounters + list(self.children):
             child.render(renderer)
     
     def reset(self):
